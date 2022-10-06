6a18a64ed5c24640afa7bd2bb9c7f74f8a82b129
shasho beyou <scplc2007@gmail.com>
2022-10-06 16:21:37 -0400

    EnName variable added in appsettings.json

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,15 +6,18 @@ namespace az_pipeline_terra.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _config;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
         {
+            _config = config;
             _logger = logger;
         }
 
@@ -28,6 +31,16 @@ namespace az_pipeline_terra.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+
+        [HttpGet]
+        [Route("getAppSettingEnv")]
+        public async Task<IActionResult> getAppSettingEnv()
+        {
+            var conString = _config["sqlConnectionString"];
+
+            return Ok(conString);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+string env;
+
+IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+env = config.GetSection("EnvName").Value;
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Configuration.AddJsonFile("appsettings.json").AddJsonFile($"appsettings.{env}.json");
 
 var app = builder.Build();
 

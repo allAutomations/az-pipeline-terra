a80b9e420547abaa1e77a29ac8164a9de9b28b63
shasho beyou <scplc2007@gmail.com>
2022-10-06 10:58:01 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source                   = "hashicorp/azurerm"
+      version                  = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "tf_rg" {
+  name                        = "az-pipeline-terra-rg"
+  location                    = "East US"
+}
+
+resource "azurerm_container_group" "tf_cg"{
+  name                        = "weatherapi"
+  location                    = azurerm_resource_group.tf_rg.location
+  resource_group_name         = azurerm_resource_group.tf_rg.name
+
+  ip_address_type             = "public"
+  dns_name_label              = "semotapis"
+  os_type                     = "Linux"
+
+  container{
+    name                      = "weatherapi"
+    image                     = "sbeyou/semotapi"
+      cpu                     = "1"
+      memory                  = "1"
+
+      ports{
+        port                  = 80
+        protocol              = "TCP"
+          }
+    }
+}

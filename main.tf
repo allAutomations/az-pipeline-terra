f3b2fe7f79504fa0dc49f38bd9b470be63578063
shasho beyou <scplc2007@gmail.com>
2022-10-06 11:07:12 -0400

    container ip address type edited

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_container_group" "tf_cg"{
   location                    = azurerm_resource_group.tf_rg.location
   resource_group_name         = azurerm_resource_group.tf_rg.name
 
-  ip_address_type             = "public"
+  ip_address_type             = "Public"
   dns_name_label              = "semotapis"
   os_type                     = "Linux"
 
